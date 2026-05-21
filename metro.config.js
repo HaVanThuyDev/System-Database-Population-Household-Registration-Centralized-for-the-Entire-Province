89654c460590786278ce0aee363125edab703d20
Hà văn thủy <168401722+HaVanThuyDev@users.noreply.github.com>
2026-05-21 09:17:01 +0700

    fix: resolve metro version conflict for expo export web build

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,6 @@
 
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-const config = {
-
-};
-
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = config;
